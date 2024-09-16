2dbc3827b5ab28673c1d623509c23a81f36f14f5
Roger Grant <rgrant@rgfm.com>
2024-09-16 13:06:59 -0400

    Fixup attendance (French vs English submit button)

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = '7Daikatana'; 
+$DatabaseName = 'opensis91'; 
+$DatabasePort = '3306'; 
+?>
