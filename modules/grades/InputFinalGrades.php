f370c54894e83941c377d18eb4fc2e3d5cdfdcc2
Roger Grant <rgrant@rgfm.com>
2026-06-22 18:01:07 -0400

    Update InputFinalGrades.php

--- modules/grades/InputFinalGrades.php
+++ modules/grades/InputFinalGrades.php
@@ -59,7 +59,8 @@ echo '<div class="panel-body">';
             foreach ($assignment_weight as $key => $weight) 
             {
                 $total+=$weight['ASSIGNMENT_WEIGHT'];
-                if($weight['ASSIGNMENT_WEIGHT']=='' || $weight['ASSIGNMENT_WEIGHT']== '0')
+#                if($weight['ASSIGNMENT_WEIGHT']=='' || $weight['ASSIGNMENT_WEIGHT']== '0')
+                if($weight['ASSIGNMENT_WEIGHT']=='')
                     $total=0;
             }
             if ($total != 100)
